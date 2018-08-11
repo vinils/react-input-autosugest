a43e12e4b44182d5be07d73ad58e1680d429dbf5
Vinícius L. Santos <vinicius.lourenco@gmail.com>
2018-08-11 19:56:59 -0300

    improving

--- src/Index.js
+++ src/Index.js
@@ -2,6 +2,6 @@ import Sugestion from './Sugestion'
 import Sugestions from './Sugestions'
 import InputAutoSugest from './InputAutoSugest'
 
-export { Sugestion, Sugestions, InputAutoSugest }
+export { Sugestion, Sugestions }
 
 export default InputAutoSugest
