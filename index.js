2a80c17df0d7660afca3fa3ace76df0cc485a507
Vinícius L. Santos <vinicius.lourenco@gmail.com>
2018-09-17 17:18:20 -0300

    Revert "adding index to use it with npm depency on github"
    This reverts commit a8c05addfe6e3cdb2e503749ed2968bae39910ca.

--- index.js
+++
@@ -1,7 +0,0 @@
-import Sugestion from './src/Sugestion'
-import Sugestions from './src/Sugestions'
-import InputAutoSugest from './src/InputAutoSugest'
-
-export { Sugestion, Sugestions }
-
-export default InputAutoSugest
