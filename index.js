a8c05addfe6e3cdb2e503749ed2968bae39910ca
Vinícius L. Santos <vinicius.lourenco@gmail.com>
2018-09-17 16:53:38 -0300

    adding index to use it with npm depency on github

---
+++ index.js
@@ -0,0 +1,7 @@
+import Sugestion from './src/Sugestion'
+import Sugestions from './src/Sugestions'
+import InputAutoSugest from './src/InputAutoSugest'
+
+export { Sugestion, Sugestions }
+
+export default InputAutoSugest
